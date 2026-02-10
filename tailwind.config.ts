c6f7e8d45f286573a09b0bed24357a5fccc0256f
Cheboy04 <cornejoadrian93@gmail.com>
2026-02-10 14:48:10 -0500

    Add Colors from previous project to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,29 @@ const config: Config = {
   			sm: 'calc(var(--radius) - 4px)'
   		},
   		colors: {
+			colors: {
+			// Paleta base
+			azul: '#001689',
+			rojo: '#ff4438',
+			azulClaro: '#4d5fb0',
+			rojoClaro: '#ff7a72',
+			grisClaro: '#f2f2f2',
+			grisOscuro: '#2e2e2e',
+			
+			// Colores oscuros
+			night: '#070B12',
+			night2: '#0B1220',
+			surface: '#0E1726',
+			surface2: '#111C2E',
+			neonBlue: '#50A0FF',
+			},
+			backgroundImage: {
+				'gradient-night': `
+				radial-gradient(1200px 600px at 10% 0%, rgba(80, 160, 255, 0.18) 0%, rgba(0,0,0,0) 55%),
+				radial-gradient(900px 500px at 90% 20%, rgba(255, 68, 56, 0.12) 0%, rgba(0,0,0,0) 60%),
+				linear-gradient(180deg, #0B1220 0%, #070B12 100%)
+				`,
+			},
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
